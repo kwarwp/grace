5678b3d455a0b96e0a08b2a00101d9601d252afe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 16:33:17 -0300

    Saved anastasia/main.py at Timestamp: 2018-05-02 19:33:17

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,6 +1,7 @@
 # grace.anastasia.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitollino.main import STYLE
+global alice_t1,alice_t2,a
 STYLE ["width"] = 900
 
 HOMEM_ARANHA = "http://4.bp.blogspot.com/-RJm2iY2ETNc/T9eVS0j1frI/AAAAAAAAFpE/BV5ZIE-UHxA/s1600/Homem-Aranha-png-Queroimagem.com+(7).png"
@@ -11,8 +12,36 @@ PANTANO = "http://img15.deviantart.net/cda6/i/2014/346/9/f/pantano_by_tetamonja-
 FINAL = "http://innhost.com.br/wp-content/uploads/2014/05/innhost_duvida.png"
 
 def tale():
+   global a, alice_t1,alice_t2
+   floresta = Cena(img=FLORESTA)
    pantano = Cena(img=PANTANO)
-   pantano.vai()
+   homem = Elemento(img=HOMEM_ARANHA, tit= "aranha", style=dict(left=100, top=100, width=100, higth=100, bottom=100))
+   homem.entra(floresta)
+   homem_t=Texto(floresta,"Saltitando pelas arvores ele tem uma visao privilegiada")
+   homem.vai=homem_t.vai
+   
+   floresta.direita = pantano
+   alice = Elemento(img=ALICE, tit= "alicinha", style=dict(left=800, top=150, width=100, higth=100, bottom=100))
+   alice.entra(pantano)
+   
+   alice_t1 = Texto(pantano,"O que sera que uma menina tao doce quanto ela estaria fazendo em um lugar tao sombrio como o pantano?")
+   alice_t2 = Texto(pantano,"SPIDER FRACO!")
+   a=0
+   def alicefala ():
+       global a,alice_t1,alice_t2
+       if a==0:
+
+       if a==1:
+          global alice_t2.vai()
+       a+=1   
+       if a>=0:
+          a=0
+   alice.vai=alicefala
+   
+
+
+   floresta.vai()
 
 tale()
+
    
