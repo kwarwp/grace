4cf789d1743cf241451f090ef3235cc527a20061
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 16:18:41 -0300

    Saved sarah/main.py at Timestamp: 2018-05-02 19:18:41

--- sarah/main.py
+++ sarah/main.py
@@ -5,7 +5,7 @@ STYLE ["width"]= 900
 
 ALICE = "https://vignette.wikia.nocookie.net/disney/images/1/1a/Alice_KH.png"
 cinderela = "http://pt-br.disneyprincesas.wikia.com/wiki/Arquivo:Tumblr_o1xpveBV601v56ooio1_500.png"
-HOMEM_ARANHA = "https://vignette.wikia.nocookie.net/marvel/images/1/17/Homem-Aranha_Artigo_Destaque.png"
+HOMEM_ARANHA = "http://3.bp.blogspot.com/-Yk4djq5lmVA/VlDjGfzAwqI/AAAAAAAAAIo/Tugx5cbl_y4/s1600/Homem%2BAranha%2BEspetacular.png"
 CIDADE = "https://png.pngtree.com/element_origin_min_pic/16/09/02/1657c93c84ebd71.jpg"
 PAIS_DE_ALICE = "https://png.pngtree.com/element_origin_min_pic/16/07/21/235790ef252fe67.jpg"
 
@@ -18,8 +18,9 @@ def texto():
     Alice.entra(cidade)
 
     cidade.direita=pais_de_alice
-    homem_aranha = Elemento(img= HOMEM_ARANHA, tit= "homem aranha", style=dict(left=150, top=290, width=150, bottom=100))
+    homem_aranha = Elemento(img= HOMEM_ARANHA, tit= "homem aranha", style=dict(left=10, top=20, width=320, bottom=100))
     cidade.vai()
+    homem_aranha.entra(pais_de_alice)
     
     
     
