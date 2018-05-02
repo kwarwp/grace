3b8f65719c372fbd985f704903769d24235d4398
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 15:36:17 -0300

    Saved lisa/main.py at Timestamp: 2018-05-02 18:36:16

--- lisa/main.py
+++ lisa/main.py
@@ -12,7 +12,7 @@ BELA_ADORMECIDA = "https://i.pinimg.com/originals/7d/ed/98/7ded98f455349d5da9bb6
 
 def tale():
     floresta = Cena(img = FLORESTA)
-    Nave = Elemento(img=NAVE, tit= nave, style=dict(right=100, top=100, width=100, hight=100, bottom=100))
+    Nave = Elemento(img=NAVE, tit= Nave, style=dict(right=100, top=100, width=100, hight=100, bottom=100))
     
     floresta.vai()
     
