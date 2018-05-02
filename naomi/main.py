50a2604e1228bc66cedf589d69d6c8540b2028d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 15:43:15 -0300

    Saved naomi/main.py at Timestamp: 2018-05-02 18:43:15

--- naomi/main.py
+++ naomi/main.py
@@ -4,7 +4,7 @@ from _spy.vitollino.main import STYLE
 STYLE ["width"]= 900
 
 
-HOEM_ARANHA = "https://2.bp.blogspot.com/-Sx288TzGsV0/V5E7WpC1S2I/AAAAAAAALNE/46iReRzHSMslkQKxOgLr6jb5Pl4LTXEnwCLcB/s1600/B78144.jpg"
+HOMEM_ARANHA = "https://2.bp.blogspot.com/-Sx288TzGsV0/V5E7WpC1S2I/AAAAAAAALNE/46iReRzHSMslkQKxOgLr6jb5Pl4LTXEnwCLcB/s1600/B78144.jpg"
 CASINHA = "http://www.meriti.rj.gov.br/iptu2018/img/casinha.png"
 ALICE = "https://2.bp.blogspot.com/-gbvgbBcZsIg/V2YHCoD6yMI/AAAAAAAAGOk/RevcsxQgQ7YhqyL5j8gDx-zrRq8MriNzACLcB/s320/1b.png"
 CASTELO = "https://vignette.wikia.nocookie.net/ouat/images/a/af/Castelo.png/revision/latest?cb=20121027032937&path-prefix=pt-br"
@@ -13,6 +13,9 @@ PANTANO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW2c7m2GcvUpqgc
 
 def nos():
    casinha = Cena(img = CASINHA)
+   homem_aranha = Elemento(img =HOMEM_ARANHA, tit= "homem_aranha", STYLE=dict(left=100, top=100, width=100, higth=100, botton=100))
+   
+   homem_aranha.entra(casinha)
    casinha.vai()
 
 nos()
