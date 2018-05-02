2614858df64c84bdbee2a2d44ac3f135beac3b98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 15:25:23 -0300

    Saved alexa/main.py at Timestamp: 2018-05-02 18:25:23

--- alexa/main.py
+++ alexa/main.py
@@ -3,14 +3,16 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitollino.main import STYLE
 STYLE ["width"]= 900
-spider = "http://pngimg.com/uploads/spider_man/spider_man_PNG89.png"
-yoda = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1rPdnuQB1ZQc6mGizdqp2sveBpW3RpiLY5zo1lVVgySb7OrKaAA"
-floresta = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIeUxwbjCdjWseCEvl6zrPev18NSEM-oUJXqGfajlKtjr2vQVi"
-pantano = "https://vignette.wikia.nocookie.net/runescape2/images/3/3f/P%C3%A2ntano_de_Mort_Myre.png/revision/latest/scale-to-width-down/640?cb=20120331195051&path-prefix=pt"
-final = "https://vignette.wikia.nocookie.net/runescape2/images/3/3f/P%C3%A2ntano_de_Mort_Myre.png/revision/latest/scale-to-width-down/640?cb=20120331195051&path-prefix=pt"
+SPIDER = "http://pngimg.com/uploads/spider_man/spider_man_PNG89.png"
+ALICE = "https://banner.kisspng.com/20180203/iuw/kisspng-alices-adventures-in-wonderland-white-rabbit-queen-alice-in-wonderland-png-free-download-5a75c9682df831.6474162315176687121883.jpg"
+YODA = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1rPdnuQB1ZQc6mGizdqp2sveBpW3RpiLY5zo1lVVgySb7OrKaAA"
+FLORESTA = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIeUxwbjCdjWseCEvl6zrPev18NSEM-oUJXqGfajlKtjr2vQVi"
+PANTANO = "https://vignette.wikia.nocookie.net/runescape2/images/3/3f/P%C3%A2ntano_de_Mort_Myre.png/revision/latest/scale-to-width-down/640?cb=20120331195051&path-prefix=pt"
+FINAL = "https://vignette.wikia.nocookie.net/runescape2/images/3/3f/P%C3%A2ntano_de_Mort_Myre.png/revision/latest/scale-to-width-down/640?cb=20120331195051&path-prefix=pt"
 
 def historia():
- pantano = Cena(img=pantano)
+ pantano = Cena(img=PANTANO)
  pantano.vai()
  
 historia()
+
