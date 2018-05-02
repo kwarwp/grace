31153e6ce252b89f041bc67804abb9a7da754456
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 15:21:25 -0300

    Saved sarah/main.py at Timestamp: 2018-05-02 18:21:24

--- sarah/main.py
+++ sarah/main.py
@@ -6,11 +6,11 @@ STYLE ["width"]= 900
 Alice = "http://disney.wikia.com/wiki/File:Alice_KH.png"
 cinderela = "http://pt-br.disneyprincesas.wikia.com/wiki/Arquivo:Tumblr_o1xpveBV601v56ooio1_500.png"
 Homem_aranha = "http://pt-br.marvel.wikia.com/wiki/Arquivo:Homem-Aranha_Artigo_Destaque.png"
-cidade = "https://png.pngtree.com/element_origin_min_pic/16/09/02/1657c93c84ebd71.jpg"
+CIDADE = "https://png.pngtree.com/element_origin_min_pic/16/09/02/1657c93c84ebd71.jpg"
 pais_de_alice = "https://png.pngtree.com/element_origin_min_pic/16/07/21/235790ef252fe67.jpg"
 
 def texto():
-    cidade = Cena(img = pantano)
+    CIDADE = Cena(img = cidade)
     cidade.vai()
     
 
