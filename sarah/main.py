20761bd436371d0d9a148ab4e63674d5db88a88c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 15:18:32 -0300

    Created sarah/main.py at Timestamp: 2018-05-02 18:18:32

---
+++ sarah/main.py
@@ -0,0 +1,16 @@
+# grace.sarah.main.py
+
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import STYLE
+STYLE ["width"]= 900
+
+Alice = "http://disney.wikia.com/wiki/File:Alice_KH.png"
+cinderela = "http://pt-br.disneyprincesas.wikia.com/wiki/Arquivo:Tumblr_o1xpveBV601v56ooio1_500.png"
+Homem_aranha = "http://pt-br.marvel.wikia.com/wiki/Arquivo:Homem-Aranha_Artigo_Destaque.png"
+cidade = "https://png.pngtree.com/element_origin_min_pic/16/09/02/1657c93c84ebd71.jpg"
+pais_de_alice = "https://png.pngtree.com/element_origin_min_pic/16/07/21/235790ef252fe67.jpg"
+
+def texto():
+    cidade = Cena(img = pantano)
+    cidade.vai()
+    
