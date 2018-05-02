46b4d36491b6e155950ab23350fea0544910b9ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 15:46:40 -0300

    Saved lisa/main.py at Timestamp: 2018-05-02 18:46:40

--- lisa/main.py
+++ lisa/main.py
@@ -4,7 +4,7 @@ from _spy.vitollino.main import STYLE
 STYLE ["width"]= 900
 
 FLORESTA = "https://img.elo7.com.br/product/zoom/10C700C/painel-floresta-g-frete-gratis-painel-impresso.jpg"
-NAVE = "http://4.bp.blogspot.com/_DRY6NerN11s/TOkTDeFZTNI/AAAAAAAAATs/YqBrhd6BZXs/s1600/nave.png"
+NAVE = "https://ugc.kn3.net/i/origin/http://servidordeimagenes1.files.wordpress.com/2013/09/flyingsaucer_zps6e5151b4.png"
 MESTRE_YODA = "https://www.allcenter.com.br/wp-content/uploads/2017/03/star-wars-yoda-sixth-scale-hot-toys-silo-902738.png"
 CASINHA_AMARELA = "https://cdn.clipte.com/u/boiperc0/14057-1xok/14057-mwaj/g1.jpg"
 CINDERELA = "https://vignette.wikia.nocookie.net/disneyprincesas/images/9/9d/Tumblr_o1xpveBV601v56ooio1_500.png/revision/latest?cb=20160710162330&path-prefix=pt-br"
@@ -12,7 +12,7 @@ BELA_ADORMECIDA = "https://i.pinimg.com/originals/7d/ed/98/7ded98f455349d5da9bb6
 
 def tale():
     floresta = Cena(img = FLORESTA)
-    nave = Elemento(img=NAVE, tit= "nave", style=dict(right=100, top=100, width=100, hight=100, bottom=100))
+    nave = Elemento(img=NAVE, tit= "nave", style=dict(left=250, top=200, width=300, hight=300, bottom=300))
     
     nave.entra(floresta)
     floresta.vai()
