d2f6212642b5f0b6240c80b1f32ee72e5aabe914
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 16:10:56 -0300

    Saved sarah/main.py at Timestamp: 2018-05-02 19:10:56

--- sarah/main.py
+++ sarah/main.py
@@ -3,18 +3,24 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitollino.main import STYLE
 STYLE ["width"]= 900
 
-ALICE = "http://disney.wikia.com/wiki/File:Alice_KH.png"
+ALICE = "https://vignette.wikia.nocookie.net/disney/images/1/1a/Alice_KH.png"
 cinderela = "http://pt-br.disneyprincesas.wikia.com/wiki/Arquivo:Tumblr_o1xpveBV601v56ooio1_500.png"
-Homem_aranha = "http://pt-br.marvel.wikia.com/wiki/Arquivo:Homem-Aranha_Artigo_Destaque.png"
+HOMEM_ARANHA = "https://vignette.wikia.nocookie.net/marvel/images/1/17/Homem-Aranha_Artigo_Destaque.png"
 CIDADE = "https://png.pngtree.com/element_origin_min_pic/16/09/02/1657c93c84ebd71.jpg"
-pais_de_alice = "https://png.pngtree.com/element_origin_min_pic/16/07/21/235790ef252fe67.jpg"
+PAIS_DE_ALICE = "https://png.pngtree.com/element_origin_min_pic/16/07/21/235790ef252fe67.jpg"
 
 def texto():
     cidade = Cena(img = CIDADE)
-    Alice = Elemento(img= ALICE, tit= "Alice", style=dict(left=100, top=100, width=100, bottom=100))
-    
+    pais_de_alice = Cena(img = PAIS_DE_ALICE)
+    Alice = Elemento(img= ALICE, tit= "Alice", style=dict(left=150, top=290, width=150, bottom=100))
+    Alice_t = Texto(cidade, "Alice foi visitar a vovó.")
+    Alice.vai = Alice_t.vai
     Alice.entra(cidade)
+
+    cidade.direita=pais_de_alice
+    homem_aranha = Elemento(img= HOMEM_ARANHA, tit= "homem aranha", style=dict(left=150, top=290, width=150, bottom=100))
     cidade.vai()
     
-
+    
+    
 texto()
