b510be9a76c79f894fe76b1f06d9688bdf5340dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-25 11:48:09 -0300

    Saved callie/main.py at Timestamp: 2018-10-25 14:48:09

--- callie/main.py
+++ callie/main.py
@@ -1,1 +1,22 @@
 # grace.callie.main.py
+from natalia.main import Mesa 
+"""
+kathryn - Baralho - Joias - Rodada
+callie - Perigo - Perder - Tesouro
+ruzwana - Ficar - Turno - Sair 
+natalia - Mesa - Iniciar - Jogador 
+lisa - Decisão - Morrer - Partida 
+meredith - Universo 
+"""
+
+
+class Perigo: 
+    def _init_(self) 
+         self.mesa = Mesa()
+         
+    def apresenta (self): 
+         self.turno.classe()
+
+
+if _name_ == "   main  "; 
+
