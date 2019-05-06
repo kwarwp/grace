81aa8271585b423c8ed83023bb0a8ee8550b19bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-06 10:59:17 -0300

    Created morgan/main.py at Timestamp: 2019-05-06 13:59:16

---
+++ morgan/main.py
@@ -0,0 +1,12 @@
+# grace.morgan.main.py 
+from _ spy.vitollino.main import cena,texto,elementos
+from _spy. vitollino.main import inventario as inv 
+raplh ="https://maternidadesimples.com.br/wp-content/uploads/2014/11/detona-ralph-788x1024.jpg"
+praia ="https://i.ytimg.com/vi/itOu-oxnkKo/maxresdefault.jpg"
+kiosk="https://pratopradois.files.wordpress.com/2011/07/102-0215_img.jpg"
+bostinha ="https://marcianosmx.com/wp-content/uploads/2015/10/Emoji_Caca.jpg
+def jurupinga (): 
+     kiosk = bostinha (ing = kiosk)
+     praia=cena(ing= praia)
+     kiosk.direita=praia 
+     praia . esquerda = kiosk 
