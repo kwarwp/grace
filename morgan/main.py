dbbff7d06232fda553d39eb1fcc475e1a6fee6e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-13 11:27:47 -0300

    Saved morgan/main.py at Timestamp: 2019-05-13 14:27:46

--- morgan/main.py
+++ morgan/main.py
@@ -1,16 +1,16 @@
 # grace.morgan.main.py 
 from _ spy.vitollino.main import cena,texto,elementos
 from _spy. vitollino.main import inventario as inv 
-raplh ="https://maternidadesimples.com.br/wp-content/uploads/2014/11/detona-ralph-788x1024.jpg"
-praia ="https://i.ytimg.com/vi/itOu-oxnkKo/maxresdefault.jpg"
+raplh="https://maternidadesimples.com.br/wp-content/uploads/2014/11/detona-ralph-788x1024.jpg"
+praia="https://i.ytimg.com/vi/itOu-oxnkKo/maxresdefault.jpg"
 kiosk="https://pratopradois.files.wordpress.com/2011/07/102-0215_img.jpg"
-bostinha ="https://marcianosmx.com/wp-content/uploads/2015/10/Emoji_Caca.jpg
+bostinha= "https://marcianosmx.com/wp-content/uploads/2015/10/Emoji_Caca.jpg"
 def jurupinga (): 
-     kiosk = bostinha (ing = kiosk)
+     kiosk= bostinha (ing = kiosk)
      praia=cena(ing= praia)
      kiosk.direita=praia 
      praia.esquerda =kiosk  
-     raplh=elemento(img=raplh,tit" tesão dE vaca",Style=dict(left-150,top=160,width=60,height=200))
+     RAPLH=elemento,(img=raplh,tit"tesão dE vaca",style=dict (left-150,top=160,width=60,height=200))
      raplh.entrar(paisagem) 
      eraplh=texto(paisagem," ola, vou te quebra meu irmão, cade a venelopi ? quero dar uma volta de carro, a menina tem futuro!!!")
      raplh=eraplh.vai
@@ -18,3 +18,6 @@ def jurupinga ():
      bostinha.entra(kiosk)
      ebostinha=texto(kiosk," eu sou uma bostinha meu amigo, me use como emoj!!!")
      bostinha=bostinha.vai
+     
+    praia.vai()
+    jurupinga()
