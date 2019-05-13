97adcdd438247f5f931dfc76da45217b023f8754
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-13 10:26:15 -0300

    Saved morgan/__log__.py at Timestamp: 2019-05-13 13:26:15

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -6,3 +6,10 @@
                                                                               ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon May 13 2019 10:26:24.963 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  bostinha ="https://marcianosmx.com/wp-content/uploads/2015/10/Emoji_Caca.jpg
+                                                                              ^
+SyntaxError: EOL while scanning string literal
+'''},
