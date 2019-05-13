e12f74c580fb72073b13c4311122f169db3689f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-13 11:17:06 -0300

    Saved morgan/__log__.py at Timestamp: 2019-05-13 14:17:04

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -62,3 +62,10 @@ SyntaxError: EOL while scanning string literal
                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 13 2019 11:17:14.403 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  raplh=elemento(img=raplh,tit" tesão dE vaca",Style=dict(left-150,top=160,width=60,height=200))
+                               ^
+SyntaxError: invalid syntax
+'''},
