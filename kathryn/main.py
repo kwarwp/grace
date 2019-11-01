1720117c03486647f55c98fe4418d7f7acd1f01e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:02:26 -0300

    Saved kathryn/main.py @2019-11-01 17:02

--- kathryn/main.py
+++ kathryn/main.py
@@ -4,8 +4,8 @@ from _spy.vitollino.main import INVENTARIO as INV
 SAKU="http://www.dlf.pt/png/big/1/11494_soldado-png.png"
 VEGETAL="https://i.pinimg.com/originals/e7/3b/2e/e73b2e5be3fa6db607b547707f48d83e.jpg"
 class brulu():
-    saku= Elemento(img=saku)
-    vegetal= Cena(img=vegetal)
+    saku= Elemento(img=SAKU)
+    vegetal= Cena(img=VEGETAL)
     saku.entra(vegetal)
     vegetal.vai
 brulu()
