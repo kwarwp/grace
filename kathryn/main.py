751b781d4c21f00fecff123e5030f151492d4cea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:00:48 -0300

    Saved kathryn/main.py @2019-11-01 17:00

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,6 +1,6 @@
 # grace.kathryn.main.py
 from _spy.vitollino.main import Cena,Elemento
-from _spy.vitollino.main import INVENTARIO INV
+from _spy.vitollino.main import INVENTARIO as INV
 SAKU="http://www.dlf.pt/png/big/1/11494_soldado-png.png"
 VEGETAL="https://i.pinimg.com/originals/e7/3b/2e/e73b2e5be3fa6db607b547707f48d83e.jpg"
 class brulu():
