160bdc1ff316cacec85a7297e9e93859e3ee5312
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:00:12 -0300

    Created kathryn/main.py @2019-11-01 17:00

---
+++ kathryn/main.py
@@ -0,0 +1,11 @@
+# grace.kathryn.main.py
+from _spy.vitollino.main import Cena,Elemento
+from _spy.vitollino.main import INVENTARIO INV
+SAKU="http://www.dlf.pt/png/big/1/11494_soldado-png.png"
+VEGETAL="https://i.pinimg.com/originals/e7/3b/2e/e73b2e5be3fa6db607b547707f48d83e.jpg"
+class brulu():
+    saku= Elemento(img=saku)
+    vegetal= Cena(img=vegetal)
+    saku.entra(vegetal)
+    vegetal.vai
+brulu()
