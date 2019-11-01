354a07b55effcb25122d434d7dfd0026a52fa843
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:00:25 -0300

    Created kathryn/__log__.py @2019-11-01 17:00

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 01 2019 13:59:10.518 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  from _spy.vitollino.main import INVENTARIO INV
+                                                ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
