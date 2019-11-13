1f530921c79313702a0edf95bd5ddd2344b22fbf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-13 18:08:05 -0300

    Saved morgan/__log__.py @2019-11-13 21:08

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -298,3 +298,21 @@ Traceback (most recent call last):
   module <module> line 3
     from _spy. vitollino.main import inventario as inv 
 '''},
+{'date': 'Wed Nov 13 2019 18:08:03.977 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    jurupinga()
+  module <module> line 10
+    kiosk= bostinha (img = kiosk)
+TypeError: 'str' object is not callable
+'''},
