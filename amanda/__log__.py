7baba56c79495fbd9cb02d4f20fc980a939bd4a7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-08 17:40:06 -0300

    Created amanda/__log__.py @2020-09-08 20:40

---
+++ amanda/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Tue Sep 08 2020 17:40:03.996 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    print(x)
+NameError: name 'x' is not defined
+'''},
