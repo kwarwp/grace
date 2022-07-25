4c90fb94031d1532a28839945d3cbd07a4c31c95
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 17:58:09 -0300

    Saved angie/__log__.py @2022-07-25 20:58

--- angie/__log__.py
+++ angie/__log__.py
@@ -22,3 +22,19 @@ SyntaxError: invalid syntax
     tesouro.vai()
 AttributeError: 'list' object has no attribute 'vai'
 '''},
+{'date': 'Mon Jul 25 2022 17:58:07.709 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    tesouro.vai()
+AttributeError: 'list' object has no attribute 'vai'
+'''},
