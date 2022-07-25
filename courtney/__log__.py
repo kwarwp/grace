1803165b407b7509b335764d0a8af87f8d7825a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:47:01 -0300

    Saved courtney/__log__.py @2022-07-25 18:47

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -36,3 +36,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Jul 25 2022 15:47:00.244 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  for camara in tumba:
+  ^
+IndentationError: unexpected indent
+'''},
