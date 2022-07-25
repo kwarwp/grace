e4c22ee257f9e737bf2967c1f0487f3f80d6c867
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 17:48:22 -0300

    Saved sara/__log__.py @2022-07-25 20:48

--- sara/__log__.py
+++ sara/__log__.py
@@ -15,3 +15,19 @@
     tumba = list[A, M, C, D, I]*3
 TypeError: __getitem__ missing 1 positional argument: 'key'
 '''},
+{'date': 'Mon Jul 25 2022 17:48:21.488 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    tumba = list[AMCDI]*3
+NameError: name 'AMCDI' is not defined
+'''},
