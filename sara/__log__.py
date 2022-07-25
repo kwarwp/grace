5fc82ca5cbdde652ca5228cba057d9fd1382db62
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 17:49:00 -0300

    Saved sara/__log__.py @2022-07-25 20:49

--- sara/__log__.py
+++ sara/__log__.py
@@ -47,3 +47,19 @@ NameError: name 'AMCDI' is not defined
     tumba = list[AMCDI]*3
 NameError: name 'AMCDI' is not defined
 '''},
+{'date': 'Mon Jul 25 2022 17:49:00.67 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    tumba = list(AMCDI)*3
+NameError: name 'AMCDI' is not defined
+'''},
