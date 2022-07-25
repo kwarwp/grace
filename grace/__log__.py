e86cbbd1f2cbea8352e81b4d7fb67ecadabf50b6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 14:01:39 -0300

    Created grace/__log__.py @2022-07-25 17:01

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jul 25 2022 14:01:37.187 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  else
+       ^
+SyntaxError: invalid syntax
+'''},
