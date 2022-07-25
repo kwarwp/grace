e399940c72f61d739547ff8e579aaf357bd395c9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:47:43 -0300

    Saved roxanne/main.py @2022-07-25 18:47

--- roxanne/main.py
+++ roxanne/main.py
@@ -4,7 +4,7 @@
 # Copyright © 2022  Carlo Oliveira <carlo@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
 jogar = input ("Quer jogar o tesouro Inca?")
-tumba = list("AMCDI")
+tumba = list("AMCDI"*3)
 if jogar == 's':
     input("simbora!")
     for camara in tumba:
