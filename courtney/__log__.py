27372a9c04bf855113b73e1e27e2cac0f2290836
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:47:58 -0300

    Saved courtney/__log__.py @2022-07-25 18:47

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -61,3 +61,10 @@ IndentationError: unexpected indent
     (cama)
 NameError: name 'cama' is not defined
 '''},
+{'date': 'Mon Jul 25 2022 15:47:57.426 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  for camara in tumba:
+  ^
+IndentationError: unexpected indent
+'''},
