035726672fc032d06e77ede4ea70ec1973ce4591
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 16:46:04 -0300

    Saved courtney/__log__.py @2022-07-25 19:46

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -174,3 +174,19 @@ NameError: name 'shuffle' is not defined
     dois_monstros = camara(camara not in tesouro) and (camara in corredor)
 TypeError: 'str' object is not callable
 '''},
+{'date': 'Mon Jul 25 2022 16:46:02.834 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    mochila += tesouro
+TypeError: unsupported operand type(s) for +: 'int' and 'list'
+'''},
