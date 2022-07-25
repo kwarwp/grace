e066bfd3e8c7d6fa9fada5cddb1fc5ea10e63cde
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:54:26 -0300

    Saved stacy/__log__.py @2022-07-25 18:54

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Jul 25 2022 15:54:26.483 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  for camara in tumba:
+  ^
+IndentationError: unexpected indent
+'''},
