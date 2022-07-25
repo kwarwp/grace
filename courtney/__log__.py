d6eab369091eba211f1604b4639d650be59216df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 16:40:20 -0300

    Saved courtney/__log__.py @2022-07-25 19:40

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -126,3 +126,19 @@ IndentationError: unexpected indent
     shuffle(tumba)
 NameError: name 'shuffle' is not defined
 '''},
+{'date': 'Mon Jul 25 2022 16:40:18.294 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    dois_monstros = camara(camara not in tesouro) and (camara in corredor)
+TypeError: 'str' object is not callable
+'''},
