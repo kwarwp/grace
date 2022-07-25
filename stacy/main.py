d2e36d424dc5bc68e4f90fd32641199224ffb783
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:43:28 -0300

    Saved stacy/main.py @2022-07-25 18:43

--- stacy/main.py
+++ stacy/main.py
@@ -3,4 +3,8 @@
 # This file is part of program Jogo Inca
 # Copyright © 2022  Claudia Motta <claudiam@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
-
+jogar = input ("Quer jogar o Tesouro Inca?")
+if jogar == "s" or "S" or "sim"or "SIM":
+    input ("Vamos nesta!")
+else:
+    input ("fica para próxima")
