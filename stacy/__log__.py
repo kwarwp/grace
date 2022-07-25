49e0e0cbb40b19608c0b2184b1e3e3b9c0a8061b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:53:40 -0300

    Created stacy/__log__.py @2022-07-25 18:53

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jul 25 2022 15:53:37.938 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  for camara in tumba:
+  ^
+IndentationError: unexpected indent
+'''},
