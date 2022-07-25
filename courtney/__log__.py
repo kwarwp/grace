9a4c34988d06e43de3b5c480ef6aebccb698fda8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 18:19:41 -0300

    Saved courtney/__log__.py @2022-07-25 21:19

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -355,3 +355,10 @@ SyntaxError: invalid syntax
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jul 25 2022 18:19:40.533 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 33
+  if tumba == 'C'
+                  ^
+SyntaxError: invalid syntax
+'''},
