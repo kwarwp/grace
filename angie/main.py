95bf83fe8be8793f13a1f8e6cb464dcea5582932
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 17:34:36 -0300

    Saved angie/main.py @2022-07-25 20:34

--- angie/main.py
+++ angie/main.py
@@ -1,7 +1,46 @@
-# grace.angie.main.py
+# grace.roxanne.main.py
 # -*- coding: UTF8 -*-
 # This file is part of program Jogo Inca
-# Copyright © 2022  Vanessa Vianna <vanmvianna@gmail.com.br>,
+# Copyright © 2022  Carlo Oliveira <carlo@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
-jogar = input ("Quer jogar o tesoura inca?")
+from random import shuffle
+from _spy.vitollino.main import Cena, STYLE
+from random import shuffle
+STYLE.update(width=1000, height=500)
+TEMPLO = "https://i.imgur.com/OOTUIwl.jpg"
+TESOURO = "https://i.imgur.com/OuPgmla.jpg"
+A = 
+M = 
+C = 
+D = 
+I = 
+templo = Cena(TEMPLO)
+cena_tesouro = Cena(TESOURO)
+templo = Cena(TEMPLO)
+jogar = input ("Quer jogar o tesouro Inca?")
+tumba = list("AMCDI"*3)
+tesouro = [1,2,3,4,5,6,7,8,9]*2
+tumba += tesouro
+shuffle(tumba)
+corredor = []
+mochila = 0
+templo.vai()
+if jogar == 's':
+    input("simbora!")
+    for camara in tumba:
+        dois_monstros = (camara not in tesouro) and (camara in corredor)
+        eh_tesouro = camara in tesouro
+        if eh_tesouro:
+            mochila += camara
+        corredor.append(camara)
+        fica = input(f"Você já encontrou {corredor}, fica(s) ou sai(n)")
+        if dois_monstros:
+            input("você perdeu")
+            mochila = 0
+            break
+        if fica != "s":
+            input(f"Voce desistiu, mas ficou com {mochila} tesouros")
+            break
+else:
+    input("pena!")
 
