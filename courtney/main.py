ccaa03ef18a4bd161192e9ed547a18cd462a2a34
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 19:54:16 -0300

    Saved courtney/main.py @2022-07-25 22:54

--- courtney/main.py
+++ courtney/main.py
@@ -1,10 +1,17 @@
 # grace.courtney.main.py
-from random import shuffle
 from _spy.vitollino.main import Cena, STYLE
 from random import shuffle
 STYLE.update(width=500, height=200)
 TEMPLO = "https://i.imgur.com/4WE57S9.jpg"
+TESOURO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgi7SJv8ULst1EochqpGSACaNpOVasSYaAOQ&usqp=CAU"
+COBRA = "https://static.vecteezy.com/ti/vetor-gratis/p3/4858121-desenho-cobra-cobra-zangado-vetor.jpg"
+ARANHA = ""
+DESABAMENTO = ""
 templo = Cena(TEMPLO)
+cenaTesouro = Cena(TESOURO)
+cobra = Cena(COBRA)
+aranha = Cena(ARANHA)
+desabamento = Cena(DESABAMENTO)
 jogar = input ("Quer jogar o tesouro Inca?")
 tumba = list("AMCDI"*3)
 tesouro = [1,2,3,4,5,6,7,8,9]*2
@@ -20,7 +27,8 @@ if jogar == 's':
         eh_tesouro = camara in tesouro
         if eh_tesouro:
             mochila += camara
-        corredor.append(camara)
+            cenaTesouro.vai()
+            corredor.append(camara)
         fica = input(f"Você já encontrou {corredor}, fica(s) ou sai(n)")
         if dois_monstros:
             input("você perdeu")
