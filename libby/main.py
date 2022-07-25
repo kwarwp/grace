0efea48387dd3ea5aec7347706faeeda874e7691
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:32:57 -0300

    Created libby/main.py @2022-07-25 18:32

---
+++ libby/main.py
@@ -0,0 +1,5 @@
+# grace.roxanne.main.py
+# -*- coding: UTF8 -*-
+# This file is part of program Jogo Inca
+# Copyright © 2022  Adriana Albuquerque <adriana.mota@nce.ufrj.br>,
+# SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
