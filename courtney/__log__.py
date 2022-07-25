5796b97758ec32360c89442cdfcb4a28bbbef46f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 17:27:43 -0300

    Saved courtney/__log__.py @2022-07-25 20:27

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -190,3 +190,19 @@ TypeError: 'str' object is not callable
     mochila += tesouro
 TypeError: unsupported operand type(s) for +: 'int' and 'list'
 '''},
+{'date': 'Mon Jul 25 2022 17:27:41.618 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    templo.vai()
+NameError: name 'templo' is not defined
+'''},
