22a0b448ed8918f229374311a30435858f4bf7bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:44:48 -0300

    Saved roxanne/main.py @2022-07-25 18:44

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,5 +3,11 @@
 # This file is part of program Jogo Inca
 # Copyright © 2022  Carlo Oliveira <carlo@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
-
-
+jogar = input ("Quer jogar o tesouro Inca?")
+tumba = list("AMCDI")
+if jogar == 's':
+    input("simbora!")
+    for camara in tumba:
+        input(f"Você encontrou {camara}")
+else:
+    input("pena!")
