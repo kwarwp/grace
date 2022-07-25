f721bbe6f3ce056fed28c105561853531157bf08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:33:33 -0300

    Created kellee/main.py @2022-07-25 18:33

---
+++ kellee/main.py
@@ -0,0 +1,5 @@
+# grace.kellee.main.py
+# -*- coding: UTF8 -*-
+# This file is part of program Jogo Inca
+# Copyright © 2022  Erica Scheffel <ericascheffel@nce.ufrj.br>,
+# SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
