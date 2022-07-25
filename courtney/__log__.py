67f2a30288e76be423f132b89cafee8aabbb459c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:37:41 -0300

    Created courtney/__log__.py @2022-07-25 18:37

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jul 25 2022 15:37:37.73 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  input('Que legal, eu também')
+  ^
+IndentationError: expected an indented block
+'''},
