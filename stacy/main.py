609703199c309e5102b622ad73daf4d503ea530a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:34:38 -0300

    Saved stacy/main.py @2022-07-25 18:34

--- stacy/main.py
+++ stacy/main.py
@@ -1,2 +1,6 @@
 # grace.stacy.main.py
+# -*- coding: UTF8 -*-
+# This file is part of program Jogo Inca
+# Copyright © 2022  Claudia Motta <claudiam@nce.ufrj.br>,
+# SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
 
