99d4daa1a2f52239e7f5c577301c8cda10a71a94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 17:45:47 -0300

    Created angie/__log__.py @2022-07-25 20:45

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jul 25 2022 17:45:44.57 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  M = 
+       ^
+SyntaxError: invalid syntax
+'''},
