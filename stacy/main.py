6b192c28661e53a092f7ce6557a7901a3c95ec94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-25 15:54:20 -0300

    Saved stacy/main.py @2022-07-25 18:54

--- stacy/main.py
+++ stacy/main.py
@@ -4,7 +4,10 @@
 # Copyright © 2022  Claudia Motta <claudiam@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
 jogar = input ("Quer jogar o Tesouro Inca?")
+tumba = lis("AMCDI"*3)
 if jogar == "s" or "S" or "sim"or "SIM":
     input ("Vamos nesta!")
+     for camara in tumba:
+        input(f"Você encontrou {camara}")
 else:
     input ("fica para próxima")
