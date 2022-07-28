bd1a05a5eb4f61f3163bb4bd3edfe57420c85838
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-28 16:36:12 -0300

    Saved courtney/__log__.py @2022-07-28 19:36

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -715,3 +715,19 @@ NameError: name 'arnha' is not defined
     if corredor[-1] == 'C':
 IndexError: list index out of range
 '''},
+{'date': 'Thu Jul 28 2022 16:36:11.935 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    if corredor[-1] == 'C':
+IndexError: list index out of range
+'''},
