b5671e42ed0bf4ac7c230062af3c2cf9763bfe2f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-29 18:25:47 -0300

    Saved angie/__log__.py @2022-07-29 21:25

--- angie/__log__.py
+++ angie/__log__.py
@@ -68,3 +68,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Jul 29 2022 18:25:46.988 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 46
+  cena_tesouro.vai()
+  ^
+IndentationError: expected an indented block
+'''},
