b68a7696e3da2cb916182f41951c51724c828594
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-29 20:23:45 -0300

    Saved angie/__log__.py @2022-07-29 23:23

--- angie/__log__.py
+++ angie/__log__.py
@@ -431,3 +431,74 @@ Traceback (most recent call last):
   module <module> line 8
     from _spy.vitollino.main import Cena, STYLE
 '''},
+{'date': 'Fri Jul 29 2022 20:23:43.64 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    from _spy.vitollino.main import Cena, STYLE
+  module _spy.vitollino.main line 147
+    from functools import WRAPPER_ASSIGNMENTS
+  module functools line 15
+    from collections import namedtuple
+  module collections line 27
+    import heapq as _heapq
+  module heapq line 130
+    from itertools import islice,count,tee,chain
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    from _spy.vitollino.main import Cena, STYLE
+ImportError: cannot import name 'Cena'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    from _spy.vitollino.main import Cena, STYLE
+  module _spy.vitollino.main line 147
+    from functools import WRAPPER_ASSIGNMENTS
+  module functools line 15
+    from collections import namedtuple
+  module collections line 27
+    import heapq as _heapq
+  module heapq line 130
+    from itertools import islice,count,tee,chain
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    from _spy.vitollino.main import Cena, STYLE
+'''},
