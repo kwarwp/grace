e26be9302329752b0495e16113113c9a384e8700
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-01 10:47:48 -0300

    Saved ruzwana/__log__.py @2022-08-01 13:47

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -64,3 +64,23 @@ TypeError: __init__() got multiple values for argument 'cena'
     self.imagem.vai()
 AttributeError: 'Camara' object has no attribute 'imagem'
 '''},
+{'date': 'Mon Aug 01 2022 10:47:47.315 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 63
+    Tesouro().vai()
+  module <module> line 60
+    Texto(self.templo, "Vai se aventurar?", A="sim", B="não", foi=self.escolheu).vai()
+  module _spy.vitollino.main line 1331
+    self.cena.elt <= Popup.POP.popup
+AttributeError: 'Camara' object has no attribute 'elt'
+'''},
