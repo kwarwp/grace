25a1702257fbf2a6f4603fc279fb99fd2ddafbcd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-01 10:27:51 -0300

    Saved ruzwana/__log__.py @2022-08-01 13:27

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -6,3 +6,23 @@
                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 01 2022 10:27:51.339 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    Tesouro().vai()
+  module <module> line 44
+    Texto("Vai se aventurar?", A="sim", B="não", foi=self.escolheu).vai()
+  module _spy.vitollino.main line 1331
+    self.cena.elt <= Popup.POP.popup
+AttributeError: 'str' object has no attribute 'elt'
+'''},
