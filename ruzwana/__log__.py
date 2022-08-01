ddd3f7f085da57ce59dc8af01daf9dc23f928156
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-01 10:27:39 -0300

    Created ruzwana/__log__.py @2022-08-01 13:27

---
+++ ruzwana/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 01 2022 10:27:37.142 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 36
+  if resposta = "A":
+               ^
+SyntaxError: invalid syntax
+'''},
