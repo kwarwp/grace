9ae2d078b0a94fe676d5a09e9f11d5dc29266765
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:00:09 -0300

    Saved angie/__log__.py @2022-08-08 18:00

--- angie/__log__.py
+++ angie/__log__.py
@@ -761,3 +761,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Aug 08 2022 15:00:08.525 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  from parisa.main import tesouro_inca as art tesouro
+                                                     ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
