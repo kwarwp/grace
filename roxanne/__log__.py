380cb4d232d6f8db8a36ce445ae6b3f1a322e1be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:27:19 -0300

    Saved roxanne/__log__.py @2022-08-08 18:27

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -31,3 +31,34 @@ NameError: name 'camera' is not defined
     cena = dict(A=Cena(ARANHA), M=Cena(MUMIA), C=Cena(COBRA), D=Cena(DESABA), I=Cena(CHAMA))
 NameError: name 'CHAMA' is not defined
 '''},
+{'date': 'Mon Aug 08 2022 15:27:18.456 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 11
+    from parisa.main import _joga_tesouro as art_joga
+ImportError: cannot import name '_joga_tesouro'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 11
+    from parisa.main import _joga_tesouro as art_joga
+'''},
