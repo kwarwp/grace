b8ce515348ff0dc6c853782c282046e145bf1949
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-07 21:43:27 -0300

    Saved kellee/__log__.py @2022-08-08 00:43

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -120,3 +120,21 @@ Traceback (most recent call last):
     dois_monstros = camera in corredor
 NameError: name 'camera' is not defined
 '''},
+{'date': 'Sun Aug 07 2022 21:42:50.599 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Então vamos!
+Clique ok para encontrar cada monstro da tumba
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    dois_monstros = camera in corredor
+NameError: name 'camera' is not defined
+'''},
