41575883b157c431bb7286921d672532dd725c48
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 17:45:52 -0300

    Saved courtney/main.py @2022-08-08 20:45

--- courtney/main.py
+++ courtney/main.py
@@ -8,41 +8,62 @@ TEMPLO = "https://i.imgur.com/4WE57S9.jpg"
 TESOURO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgi7SJv8ULst1EochqpGSACaNpOVasSYaAOQ&usqp=CAU"
 COBRA = "https://static.vecteezy.com/ti/vetor-gratis/p3/4858121-desenho-cobra-cobra-zangado-vetor.jpg"
 ARANHA = "https://i.imgur.com/Dizbyyi.jpg"
-from parisa.main import tesouro_inca as art_tesouro
-from random import choice
-criptas = []
 
+from parisa,main import tesouro_inca
+from random import choice
 class Camara:
-    def __init__(self, conteudo, valor = 0):
+    def __init__(self, conteudo, valor=0):
         self.conteudo = conteudo
         self.valor = valor
-    def revela
+    def revela(self):
+        return self.conteudo
+    def resgata(self, jogadores):
+        butim, sobra = self.valor // jogadores, self.valor % jogadores
+        self.valor = sobra
+        return butim
+    def __repr__(self):
+        return self.conteudo
+        
+class Incursao:
+    pass        
+class Tumba:
+    pass
+class Jogador:
+    pass
 
-class jogaTesouro():
+class JogaTesouro():
     """Inicia o jogo do tesouro inca"""
     def __init__(self):
         self.criptas = []
-        camaras = ['Aranha','Cobra', 'Mumia', 'Desabamento', 'Incendio']
+        self.camaras = ["Aranha", "Múmia", "Cobra", "Desabamento", "Incêndio"]
+        self.camaras = [Camara(contem) for contem in self.camaras]
+        os_tesouros = [1,2,3,4,5,5, 7,7,9, 11,11,13,14, 15,17]
+        os_tesouros = [Camara(valor, valor) for valor in os_tesouros]
+        self.camaras += os_tesouros
+        
     def joga(self):
-        camara = choice(camaras)
+        camara = choice(self.camaras)
         tinha_monstro = camara in self.criptas
         self.criptas.append(camara)
+        revela = [cam.revela() for cam in self.criptas]
+        cam= camara.revela()
         if tinha_monstro:
-            input(f'Já existia {camara} na {self.criptas}. Vocêsaiu correndo')
-            return 'n'
-        return input('Você visitou {self.criptas} e achou {camara}. Continua(s)?')
-
+            input(f"Ja existia {cam} na {revela}. Você saiu correndo")
+            return "n"
+        return input(f"Você visitou {revela} achou {cam}. Continua(s)")
 
 def tesouro_inca():
-    """O jogo do tesouro Inca"""
-    quer = input(f'Bem vindo ao Tesouro Inca - versão Courtney {__name__} \n Quer jogar(s)?')
-    fala = 'beleza' if quer =='s' else 'que triste'
+    """O jogo do Tesouro Inca"""
+    quer = input(f"Bem vindo ao Tesouro Inca - versão Courtney /n {__name__}.Quer jogar (s)?")
+    fala = "beleza" if quer == "s" else "que triste"
     input(fala)
-    while _joga_tesouro() == 's':
+    _joga_tesouro = JogaTesouro()
+    while _joga_tesouro.joga() == "s":
         pass
-    fala = 'poxa, que pena'
+    fala = "que pena"
     input(fala)
-    joga_tesouro = jogaTesouro()
-if __name__ == "__main__":    
-    #art_tesouro()
+    
+
+if __name__ == "__main__":
+    # art_tesouro()
     tesouro_inca()
