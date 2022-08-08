f5d1688f9ffc55f531284571312115f8774cb8e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 16:37:10 -0300

    Saved parisa/__log__.py @2022-08-08 19:37

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -564,3 +564,21 @@ NameError: name 'criptas' is not defined
     tinha_monstro = camara in criptas
 NameError: name 'criptas' is not defined
 '''},
+{'date': 'Mon Aug 08 2022 16:37:10.74 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    tesouro_inca()
+  module <module> line 44
+    while joga_tesouro() == "s":
+NameError: name 'joga_tesouro' is not defined
+'''},
