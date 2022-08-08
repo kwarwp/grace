da138f54d50600ffb2c7436b1083a7929d797aa3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 17:02:48 -0300

    Saved parisa/__log__.py @2022-08-08 20:02

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -670,3 +670,10 @@ SyntaxError: invalid syntax
     self.camaras = [camara(contem) for contem in self.camaras]
 NameError: name 'camara' is not defined
 '''},
+{'date': 'Mon Aug 08 2022 17:02:46.957 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 34
+  def __str__(self):
+  ^
+IndentationError: unexpected indent
+'''},
