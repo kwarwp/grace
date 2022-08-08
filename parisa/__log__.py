05444e8c8dc4b5b425c0b13c3d876613b3042fec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:13:06 -0300

    Saved parisa/__log__.py @2022-08-08 18:13

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 08 2022 15:13:06.576 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  def _joga_tesouro
+                    ^
+SyntaxError: invalid syntax
+'''},
