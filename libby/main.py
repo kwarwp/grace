5f8b6768a81bd515e6240a70326809f8ddb49e8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:41:23 -0300

    Saved libby/main.py @2022-08-08 18:41

--- libby/main.py
+++ libby/main.py
@@ -16,7 +16,7 @@ def _joga_tesouro():
     camaras = ["Aranha", "Múmia", "Cobra", "Desabamento", "Incêndio"]
     camara = choice(camaras)
     criptas.append (camara)
-    return input(f"Você achou {criptas}. Continua(s)")
+    return input(f"Você achou {criptas} achou {camara}. Continua(s)")
 
 def tesouro_inca():
     """O jogo do Tesouro Inca"""
