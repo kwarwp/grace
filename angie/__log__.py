8ef523a042bb2e98a4181eef30dd5f7eec8183e8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 17:49:55 -0300

    Saved angie/__log__.py @2022-08-08 20:49

--- angie/__log__.py
+++ angie/__log__.py
@@ -1142,3 +1142,23 @@ IndentationError: unexpected indent
     self.camaras = [Camara(contem) for contem in self.camaras] * 3
 TypeError: object() takes no parameters
 '''},
+{'date': 'Mon Aug 08 2022 17:49:53.559 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 77
+    tesouro_inca()
+  module <module> line 67
+    _joga_tesouro = JogaTesouro()
+  module <module> line 42
+    self.camaras = [Camara(contem) for contem in self.camaras] * 3
+TypeError: object() takes no parameters
+'''},
