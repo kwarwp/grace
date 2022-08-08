3d4319c68db67fd62e1bd7483e7b5b6f69cc33f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 16:03:19 -0300

    Saved angie/__log__.py @2022-08-08 19:03

--- angie/__log__.py
+++ angie/__log__.py
@@ -960,3 +960,21 @@ SyntaxError: invalid syntax
 'error': '''
 Exception: <TypeError: undefined has no properties>
 '''},
+{'date': 'Mon Aug 08 2022 16:03:17.403 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    tesouro_inca()
+  module <module> line 36
+    while art_joga() == "s":
+NameError: name 'art_joga' is not defined
+'''},
