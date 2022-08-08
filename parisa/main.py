41ce1ffa6eb81f215bcc386fb7e1d8c740b88886
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:31:23 -0300

    Saved parisa/main.py @2022-08-08 17:31

--- parisa/main.py
+++ parisa/main.py
@@ -4,6 +4,11 @@
 # Copyright © 2022  Art Adriel <art.adriel@gmail.com>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
 
+TEMPLO = "https://i.imgur.com/OOTUIwl.jpg"
+TESOURO = "https://i.imgur.com/OuPgmla.jpg"
+MONSTRO = "https://i.imgur.com/lcvvL1B.png"
+PERIGOS = "p2jHT5d ho6ZMAL G2u6KB1 awwkaBO hZ0ohTz"
+
 def tesouro_inca():
 	"""O jogo do Tesouro Inca"""
 	input("Bem vindo ao Tesouro Inca - versão Parisa")
