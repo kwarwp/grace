8aa7096d2e974a6327a19bfdd27e4dcdee48118f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:16:29 -0300

    Saved roxanne/main.py @2022-08-08 18:16

--- roxanne/main.py
+++ roxanne/main.py
@@ -7,13 +7,25 @@ TEMPLO = "https://i.imgur.com/OOTUIwl.jpg"
 TESOURO = "https://i.imgur.com/OuPgmla.jpg"
 MONSTRO = "https://i.imgur.com/lcvvL1B.png"
 PERIGOS = "p2jHT5d ho6ZMAL G2u6KB1 awwkaBO hZ0ohTz"
-from parisa.main import tesouro_inca
+from parisa.main import tesouro_inca as art_tesouro
 
-def _tesouro_inca():
+def _joga_tesouro():
+    """Inicia o jogo do tesouro inca"""
+    return input("Você achou Aranha. Continua(s)")
+
+def tesouro_inca():
     """O jogo do Tesouro Inca"""
-    input(f"Bem vindo ao Tesouro Inca - versão Roxanne {__name__}")
+    quer = input(f"Bem vindo ao Tesouro Inca - versão Roxanne {__name__}.Quer jogar (s)?")
+    fala = "beleza" if quer == "s" else "que triste"
+    input(fala)
+    while _joga_tesouro() == "s":
+        fala = "beleza"
+        input(fala)
+    fala = "que pena"
+    input(fala)
     
 
 if __name__ == "__main__":
+    # art_tesouro()
     tesouro_inca()
 
