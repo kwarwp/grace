9daaa612a9969254f8fecf04d2545158591be2e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:50:42 -0300

    Created parisa/__log__.py @2022-08-08 17:50

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 08 2022 14:50:41.209 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  input(f"Bem vindo ao Tesouro Inca - versão Parisa"{__name__})
+                                                     ^
+SyntaxError: invalid syntax
+'''},
