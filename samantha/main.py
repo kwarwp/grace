7613efa845ab45e10e9dbe01ddc6f24c6646d49c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 10:08:38 -0300

    Created samantha/main.py @2022-08-08 13:08

---
+++ samantha/main.py
@@ -0,0 +1,11 @@
+# grace.samantha.main.py
+# -*- coding: UTF8 -*-
+# This file is part of program Jogo Inca
+# Copyright © 2022  Carlo Oliveira <carlo@nce.ufrj.br>,
+# SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
+def main(br=None):
+    input("vamos jogar?")
+    
+    
+if __name__ == "__main__":
+    main()
