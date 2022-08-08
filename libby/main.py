7f9abc91c94969c437691c7e1faa52eb7eb61b5b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:31:48 -0300

    Saved libby/main.py @2022-08-08 17:31

--- libby/main.py
+++ libby/main.py
@@ -3,3 +3,14 @@
 # This file is part of program Jogo Inca
 # Copyright © 2022  Adriana Albuquerque <adriana.mota@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
+TEMPLO = "https://i.imgur.com/OOTUIwl.jpg"
+TESOURO = "https://i.imgur.com/OuPgmla.jpg"
+MONSTRO = "https://i.imgur.com/lcvvL1B.png"
+PERIGOS = "p2jHT5d ho6ZMAL G2u6KB1 awwkaBO hZ0ohTz"
+
+def tesouro_inca():
+    """O jogo do Tesouro Inca"""
+    input("Bem vindo ao Tesouro Inca - versão Libby")
+    
+    
+tesouro_inca()
