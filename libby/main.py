9b258aa82a44d7fece586738e75bf82cd660ebb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 17:23:53 -0300

    Saved libby/main.py @2022-08-08 20:23

--- libby/main.py
+++ libby/main.py
@@ -45,14 +45,13 @@ class JogaTesouro ():
     self.criptas = []
     self.camaras = ["Aranha", "Múmia", "Cobra", "Desabamento", "Incêndio"]
     self.camaras = [camara(contem) for contem in self.camaras]
-    self.os_tesouros = [1,2,3,4,5,5,7,7,8,9,10,11,11,12,13,14,15,17]
     os_tesouros = [1,2,3,4,5,5,7,7,8,9,10,11,11,12,13,14,15,17]
     self.os_tesouros = [camara(valor, valor ) for valor in os_tesouros]
-    self.camaras += os_tesouros
+    self.camaras += self.os_tesouros
     
     def joga(self):
         camara = choice(self.camaras)
-        tinha_monstro = camara in self.criptas
+        tinha_monstro = (camara in self.criptas)
         self.criptas.append(camara)
         revela = [cam.revela() for cam in self criptas]
         cam = camara.revela
