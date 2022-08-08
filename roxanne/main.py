b12906630538246b67c758dd7fdb164ca40b9655
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 17:17:16 -0300

    Saved roxanne/main.py @2022-08-08 20:17

--- roxanne/main.py
+++ roxanne/main.py
@@ -29,6 +29,8 @@ class Camara:
         butim, sobra = self.valor // jogadores, self.valor % jogadores
         self.valor = sobra
         return butim
+    def __repr__(self):
+        return self.conteudo
         
 class Incursao:
     pass        
@@ -43,15 +45,20 @@ class JogaTesouro():
         self.criptas = []
         self.camaras = ["Aranha", "Múmia", "Cobra", "Desabamento", "Incêndio"]
         self.camaras = [Camara(contem) for contem in self.camaras]
+        os_tesouros = [1,2,3,4,5,5, 7,7,9, 11,11,13,14, 15,17]
+        os_tesouros = [Camara(valor, valor) for valor in os_tesouros]
+        self.camaras += os_tesouros
         
     def joga(self):
         camara = choice(self.camaras)
         tinha_monstro = camara in self.criptas
         self.criptas.append(camara)
+        revela = [cam.revela() for cam in self.criptas]
+        cam= camara.revela()
         if tinha_monstro:
-            input(f"Ja existia {camara} na {self.criptas}. Você abandonou correndo")
+            input(f"Ja existia {cam} na {revela}. Você abandonou correndo")
             return "n"
-        return input(f"Você visitou {self.criptas} achou {camara}. Continua(s)")
+        return input(f"Você visitou {revela} achou {cam}. Continua(s)")
 
 def tesouro_inca():
     """O jogo do Tesouro Inca"""
