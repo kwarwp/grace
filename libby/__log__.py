b8c0203ec5c7207ac1a1b408f8d82e92919be2d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:31:16 -0300

    Saved libby/__log__.py @2022-08-08 18:31

--- libby/__log__.py
+++ libby/__log__.py
@@ -49,3 +49,23 @@ NameError: name 'fala' is not defined
     camara = choice(camaras)
 NameError: name 'choice' is not defined
 '''},
+{'date': 'Mon Aug 08 2022 15:31:14.855 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    tesouro_inca()
+  module <module> line 23
+    while _joga_tesouro() == "s":
+  module <module> line 15
+    camara = choice(camaras)
+NameError: name 'choice' is not defined
+'''},
