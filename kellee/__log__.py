bd0fe608f4a7a1f455b46a9d032c52154e35184c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:25:07 -0300

    Saved kellee/__log__.py @2022-08-08 18:25

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -564,3 +564,23 @@ NameError: name 'camera' is not defined
     dois_monstros = (camara not in tesouro) and (camera in corredor)
 NameError: name 'camera' is not defined
 '''},
+{'date': 'Mon Aug 08 2022 15:24:30.387 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    tesouro_inca()
+  module <module> line 33
+    while _joga_tesouro() == "s":
+  module <module> line 25
+    camara = choice(camara)
+UnboundLocalError: local variable 'camara' referenced before assignment
+'''},
