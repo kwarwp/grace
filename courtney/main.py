7a1de32265b207387ca14088260e6a5a45f00777
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:02:40 -0300

    Saved courtney/main.py @2022-08-08 18:02

--- courtney/main.py
+++ courtney/main.py
@@ -8,10 +8,11 @@ TEMPLO = "https://i.imgur.com/4WE57S9.jpg"
 TESOURO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgi7SJv8ULst1EochqpGSACaNpOVasSYaAOQ&usqp=CAU"
 COBRA = "https://static.vecteezy.com/ti/vetor-gratis/p3/4858121-desenho-cobra-cobra-zangado-vetor.jpg"
 ARANHA = ""
+from parisa.main import tesouro_inca
 
 def tesouro_inca():
     """O jogo do tesouro Inca"""
-    input('Bem vindo ao Tesouro Inca - versão Courtney')
+    input(f'Bem vindo ao Tesouro Inca - versão Courtney {__name__}')
     
-    
-tesouro_inca()
+if __name__ == "__main__":    
+    tesouro_inca()
