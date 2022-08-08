646f69cdfb498dfc9336f0936fd1542749868c10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:07:49 -0300

    Saved libby/__log__.py @2022-08-08 18:07

--- libby/__log__.py
+++ libby/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Aug 08 2022 15:07:49.199 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  quer = input(f"Bem vindo ao Tesouro Inca - versão Libby {__name__}. Quer jogar (s)?")
+  ^
+IndentationError: unexpected indent
+'''},
