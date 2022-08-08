bf0007b7033ec1dde9d1d6ac3b9d804fbf83d377
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:29:50 -0300

    Saved parisa/main.py @2022-08-08 17:29

--- parisa/main.py
+++ parisa/main.py
@@ -1,5 +1,12 @@
 # grace.parisa.main.py
 # -*- coding: UTF8 -*-
 # This file is part of program Jogo Inca
-# Copyright © 2022  nome <mail>,
+# Copyright © 2022  Art Adriel <art.adriel@gmail.com>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
+def tesouro_inca()
+	"""O jogo do Tesouro Inca"""
+input("Bem vindo ao Tesouro Inca - versão Roxanne")
+pass 
+
+
+tesouro_inca()
