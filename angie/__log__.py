2afab919d2689b1d34f5143e7bd9c6d3a0c7e266
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:25:27 -0300

    Saved angie/__log__.py @2022-08-08 17:25

--- angie/__log__.py
+++ angie/__log__.py
@@ -715,3 +715,19 @@ Traceback (most recent call last):
   module <module> line 8
     from _spy.vitollino.main import Cena, STYLE
 '''},
+{'date': 'Mon Aug 08 2022 14:25:25.932 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    tesouro_inca()
+NameError: name 'tesouro_inca' is not defined
+'''},
