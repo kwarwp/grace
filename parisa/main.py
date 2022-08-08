71b3c90401b2c6a330ea6e5367e2374a0fc63086
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:28:44 -0300

    Created parisa/main.py @2022-08-08 17:28

---
+++ parisa/main.py
@@ -0,0 +1,5 @@
+# grace.parisa.main.py
+# -*- coding: UTF8 -*-
+# This file is part of program Jogo Inca
+# Copyright © 2022  nome <mail>,
+# SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
