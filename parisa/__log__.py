d526434b35f7adb0c5a5652b1066a642d3e2c0ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:53:16 -0300

    Saved parisa/__log__.py @2022-08-08 18:53

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -195,3 +195,10 @@ SyntaxError: invalid syntax
                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 08 2022 15:53:16.109 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  if camara[0] == "A"
+                      ^
+SyntaxError: invalid syntax
+'''},
