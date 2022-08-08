14f257ea34d89bc6bcb5ede8b6e45170b9fb73ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:29:26 -0300

    Saved angie/__log__.py @2022-08-08 18:29

--- angie/__log__.py
+++ angie/__log__.py
@@ -853,3 +853,19 @@ ImportError: cannot import name 'tesouro_inca'
     from parisa.main import tesouro_inca as art_tesouroo
 ImportError: cannot import name 'tesouro_inca'
 '''},
+{'date': 'Mon Aug 08 2022 15:29:25.312 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from parisa.main import tesouro_inca as art_tesouro
+ImportError: cannot import name 'tesouro_inca'
+'''},
