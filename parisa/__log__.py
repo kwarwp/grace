f6cc9d5f182f5fcc244dcabccb71771227998239
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:54:02 -0300

    Saved parisa/__log__.py @2022-08-08 18:54

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -216,3 +216,10 @@ SyntaxError: invalid syntax
                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 08 2022 15:54:02.737 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  if criptas.append(camara[0])=="A"
+  ^
+IndentationError: unexpected indent
+'''},
