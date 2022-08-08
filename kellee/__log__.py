667d438247111cfe7890bb41e78a998b4743d5f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 17:28:43 -0300

    Saved kellee/__log__.py @2022-08-08 20:28

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -746,3 +746,23 @@ AttributeError: 'set' object has no attribute '__getitem__'
     camara = choice(self.camaras)    
 AttributeError: 'set' object has no attribute '__getitem__'
 '''},
+{'date': 'Mon Aug 08 2022 17:28:06.784 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 79
+    tesouro_inca()
+  module <module> line 71
+    _joga_tesouro = JogaTesouro()
+  module <module> line 52
+    os_tesouros = [Camara(valor, valor) for valor in os_tesouros]
+NameError: free variable 'os_tesouros' referenced before assignment in enclosing scope
+'''},
