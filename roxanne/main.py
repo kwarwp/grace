0ced2729718b94b6049091db65eaa37de8fe6964
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:11:53 -0300

    Saved roxanne/main.py @2022-08-08 17:11

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,3 +3,7 @@
 # This file is part of program Jogo Inca
 # Copyright © 2022  Carlo Oliveira <carlo@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
+TEMPLO = "https://i.imgur.com/OOTUIwl.jpg"
+TESOURO = "https://i.imgur.com/OuPgmla.jpg"
+MONSTRO = "https://i.imgur.com/lcvvL1B.png"
+PERIGOS = "p2jHT5d ho6ZMAL G2u6KB1 awwkaBO hZ0ohTz"
