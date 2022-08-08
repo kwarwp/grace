1c6350883342533a5d32099cf06b40c306767226
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 16:12:18 -0300

    Saved parisa/__log__.py @2022-08-08 19:12

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -496,3 +496,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Aug 08 2022 16:12:18.179 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 32
+  if camara in cripta == [A]:
+  ^
+IndentationError: unexpected indent
+'''},
