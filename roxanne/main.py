9e4f746e492d2df62a0030b023cf24777355f780
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 14:22:42 -0300

    Saved roxanne/main.py @2022-08-08 17:22

--- roxanne/main.py
+++ roxanne/main.py
@@ -7,3 +7,11 @@ TEMPLO = "https://i.imgur.com/OOTUIwl.jpg"
 TESOURO = "https://i.imgur.com/OuPgmla.jpg"
 MONSTRO = "https://i.imgur.com/lcvvL1B.png"
 PERIGOS = "p2jHT5d ho6ZMAL G2u6KB1 awwkaBO hZ0ohTz"
+
+def tesouro_inca():
+    """O jogo do Tesouro Inca"""
+    pass
+    
+    
+tesouro_inca()
+
