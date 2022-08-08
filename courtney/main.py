969b0c092f79ffa2a2b0d1b966b575c078cd36e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:25:54 -0300

    Saved courtney/main.py @2022-08-08 18:25

--- courtney/main.py
+++ courtney/main.py
@@ -7,12 +7,15 @@
 TEMPLO = "https://i.imgur.com/4WE57S9.jpg"
 TESOURO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgi7SJv8ULst1EochqpGSACaNpOVasSYaAOQ&usqp=CAU"
 COBRA = "https://static.vecteezy.com/ti/vetor-gratis/p3/4858121-desenho-cobra-cobra-zangado-vetor.jpg"
-ARANHA = ""
+ARANHA = "https://i.imgur.com/Dizbyyi.jpg"
 from parisa.main import tesouro_inca as art_tesouro
+from random import choice
 
 def _joga_tesouro():
     """Inicia o jogo do tesouro inca"""
-    return input('Vocêachou aranha. Continua(s)?')
+    camaras = ['Aranha','Cobra', 'Mumia', 'Desabamento', 'Incendio']
+    camara = choice(camaras)
+    return input(f'Você achou {camara}. Continua(s)?')
 
 def tesouro_inca():
     """O jogo do tesouro Inca"""
