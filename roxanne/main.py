45faed2d2e65d6cc70b3b3c47e6158c13f8ff573
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 16:11:23 -0300

    Saved roxanne/main.py @2022-08-08 19:11

--- roxanne/main.py
+++ roxanne/main.py
@@ -27,7 +27,7 @@ def _joga_tesouro():
     tinha_monstro = camara in criptas
     criptas.append(camara)
     if tinha_monstro:
-        input(f"Ja existia {camara} na {cripats}. Você abandonou correndo")
+        input(f"Ja existia {camara} na {criptas}. Você abandonou correndo")
         return "n"
     return input(f"Você visitou {criptas} achou {camara}. Continua(s)")
 
