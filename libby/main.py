81a7621cb3f1670c144f55f48af7bf243557fd48
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-08 15:18:09 -0300

    Saved libby/main.py @2022-08-08 18:18

--- libby/main.py
+++ libby/main.py
@@ -9,14 +9,22 @@ MONSTRO = "https://i.imgur.com/lcvvL1B.png"
 PERIGOS = "p2jHT5d ho6ZMAL G2u6KB1 awwkaBO hZ0ohTz"
 from parisa.main import tesouro_inca as art_tesouro
 
+def _joga_tesouro():
+    """Inicia o jogo do tesouro inca"""
+    return input("Você achou Aranha. Continua(s)")
+
 def tesouro_inca():
     """O jogo do Tesouro Inca"""
-    quer = input(f"Bem vindo ao Tesouro Inca - versão Libby {__name__}. Quer jogar (s)?")
-    fala = "beleza" if quer == "s" else "que pena"
+    quer = input(f"Bem vindo ao Tesouro Inca - versão Libby {__name__}.Quer jogar (s)?")
+    fala = "Legal" if quer == "s" else "que pena"
     input(fala)
-    continua = _joga_tesouro ()
-    fala = "beleza" if quer == "s" else "que pena"
+    while _joga_tesouro() == "s":
+        fala = "legal"
+        input(fala)
+    fala = "que pena"
     input(fala)
     
+
 if __name__ == "__main__":
+    # art_tesouro()
     tesouro_inca()
