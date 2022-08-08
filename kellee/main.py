e9e2edee30d0b1351449200c2cf3703737b678be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-07 22:01:02 -0300

    Saved kellee/main.py @2022-08-08 01:01

--- kellee/main.py
+++ kellee/main.py
@@ -4,26 +4,48 @@
 # Copyright © 2022  Erica Scheffel <ericascheffel@nce.ufrj.br>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
 from random import shuffle
+from _spy.vitollino.main import Cena, STYLE
+TEMPLO = "https://i.imgur.com/OOTUIwl.jpg"
+TESOURO = "https://i.imgur.com/OuPgmla.jpg"
+MONSTRO = "https://i.imgur.com/lcvvL1B.png"
+ARANHA = "https://i.imgur.com/p2jHT5d.png"
+COBRA = "https://i.imgur.com/ho6ZMAL.png"
+CHAMAS = "https://i.imgur.com/G2u6KB1.jpg"
+MUMIA = "https://i.imgur.com/awwkaBO.jpg"
+DESABA = "https://i.imgur.com/hZ0ohTz.jpg"
+templo = Cena(TEMPLO)
+cena_tesouro = Cena(TESOURO)
+cena_monstro = Cena(MONSTRO)
 jogar = input("Quer jogar o Tesouro Inca?")
 tumba = list("AMCDI"*3)
 tesouro = [1,2,3,4,5,6,7,8,9]*2
 tumba += tesouro
 shuffle(tumba)
 corredor = []
+mochila = 0
+templo.vai()
 if (jogar == 's' or 'S' or "sim" or "SIM" or "Sim"):
     print("Então vamos!")
     print("Clique ok para encontrar cada monstro da tumba")
     input("Simbora")
     for camara in tumba:
-        dois_monstros = camara in corredor
+        dois_monstros = (camara not in tesouro) and (camera in corredor)
+        eh_tesouro = camara in tesouro
+        if eh_tesouro:
+            mochila += camara
+            cena_tesouro.vai()
+        else:
+            cena_monstro.vai()
+            
         corredor.append(camara)        
         fica = input(f"você encontrou {corredor}, você fica?")
         if (fica != 's' or 'S' or "sim" or "SIM" or "Sim"):
-            input("Você desistiu")
+            input("Você desistiu, mas ficou com {mochila} tesouros")
             break
         
         if dois_monstros:
             input("Você perdeu")
+            mochila = 0
             break
 else:
     print("Que pena")
