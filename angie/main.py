85470bdd76f8d6261987b8a1a971b79f28c764c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-09 12:13:25 -0300

    Saved angie/main.py @2022-08-09 15:13

--- angie/main.py
+++ angie/main.py
@@ -3,7 +3,7 @@
 # This file is part of program Jogo Inca
 # Copyright © 2022  Vanessa Vianna <vanmvianna@gmail.com>,
 # SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
-"""IMAGENS"""
+"""importacao de imagens"""
 TEMPLO = "https://i.imgur.com/DuyTTha.jpg"
 TESOURO = "https://i.imgur.com/OuPgmla.jpg"
 A = "https://imgur.com/iCrJ46A"
@@ -12,15 +12,26 @@ F = "https://imgur.com/xYv9YdG"
 C = "https://i.imgur.com/8VfAotu.jpg"
 D = "https://i.imgur.com/OHmUIhz.jpg"
 
+""" O Jogo do Tesouro Inca """
+
+"""importacao de bibliotecas"""
 from random import choice, shuffle
 
+"""classes/módulos""""
 class Camara:
+    """ Representa uma camara do templo, conteudo: perigo ou tesouro, valor: o número de tesouros"""
     def __init__(self, conteudo, valor=0):
         self.conteudo = conteudo
         self.valor = valor
     def revela(self):
+        """ Mostra o conteúdo que estava oculto na câmara"""
         return self.conteudo
     def resgata(self, jogadores):
+        """ Divide o conteúdo que estava oculto na câmara por um grupo de aventureiros
+        
+            :param: jogadores: o número de aventureiros que vão dividir o tesouro
+            :return: o número de tesouros que cabe a cada aventureiro
+        """
         butim, sobra = self.valor // jogadores, self.valor % jogadores
         self.valor = sobra
         return butim
@@ -28,6 +39,7 @@ class Camara:
         return self.conteudo
         
 class Incursao:
+"""Representa uma Incursão no templo"""
     pass        
 class Tumba:
     pass
@@ -35,16 +47,24 @@ class Jogador:
     pass
 
 class JogaTesouro():
+    """Inicia o jogo do tesouro inca e avança na cripta """
     def __init__(self):
         self.criptas = []
-        self.camaras = ["Aranha", "Múmia", "Cobra", "Desabamento", "Incêndio"]
-        self.camaras = [Camara(contem) for contem in self.camaras] * 3
+        """Camaras já reveladas pelos aventureiros """
+        camaras = ["Aranha", "Múmia", "Cobra", "Desabamento", "Incêndio"]
+        self.camaras = [Camara(contem) for contem in camaras] * 3
+        """Camaras contidas no Templo """
         os_tesouros = [1,2,3,4,5,5, 7,7,9, 11,11,13,14, 15,17]
         self.os_tesouros = [Camara(valor, valor) for valor in os_tesouros]
+        """Camaras contidas no Templo que contêm tesouros"""
         self.camaras += self.os_tesouros
         shuffle(self.camaras)
         
     def joga(self):
+        """Executa uma jogada, revelando uma câmara 
+        
+           :return: Se o jogo continua, retorna "s"
+        """
         # camara = choice(self.camaras)
         camara = self.camaras.pop()
 
@@ -58,7 +78,7 @@ class JogaTesouro():
         return input(f"Você visitou {revela} achou {cam}. Continua(s)")
 
 def tesouro_inca():
-    """O jogo do Tesouro Inca"""
+    """executa O jogo do Tesouro Inca"""
     quer = input(f"Bem vindo ao Tesouro Inca - versão Roxanne {__name__}.Quer jogar (s)?")
     fala = "beleza" if quer == "s" else "que triste"
     input(fala)
