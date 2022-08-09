000ab0c8955d8526022ce62561e90cac59a69027
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-09 19:28:28 -0300

    Saved angie/main.py @2022-08-09 22:28

--- angie/main.py
+++ angie/main.py
@@ -29,7 +29,7 @@ cena_Incendio = Cena(imagem_F)
 cena_Cobra = Cena(imagem_C)
 cena_Desmoronamento = Cena(imagem_D)
 
-"""classe / modulos"""
+"módulos/classe"
 class Camara:
     def __init__(self, conteudo, valor=0):
         self.conteudo = conteudo
@@ -41,13 +41,12 @@ class Camara:
         self.valor = sobra
         return butim
     def __repr__(self):
-        return self.conteudo
+        return self.conteudo        
 
 class JogaTesouro():
-     
     def __init__(self):
         self.criptas = []
-        camaras = ["Aranha", "Mumia", "Cobra", "Desmoronamento", "Incendio"]
+        camaras = ["Aranha", "Mumia", "Cobra", "Desabamento", "Incendio"]
         self.camaras = [Camara(contem) for contem in camaras] * 3
         os_tesouros = [1,2,3,4,5,5, 7,7,9, 11,11,13,14, 15,17]
         self.os_tesouros = [Camara(valor, valor) for valor in os_tesouros]
@@ -55,7 +54,7 @@ class JogaTesouro():
         shuffle(self.camaras)
         
     def joga(self):
-        camara = self.camaras.pop()
+        camara = self.camaras.pop
         tinha_monstro = (camara not in self.os_tesouros) and (camara in self.criptas) 
         self.criptas.append(camara)
         revela = [cam.revela() for cam in self.criptas]
@@ -64,17 +63,19 @@ class JogaTesouro():
             input(f"Ja existia {cam} na {revela}. Você abandonou correndo")
             return "n"
         return input(f"Você visitou {revela} achou {cam}. Continua(s)")
-             
-    def tesouro_inca():
-        quer = input(f"Bem vindo ao Tesouro Inca - versão Roxanne {__name__}.Quer jogar (s)?", cena_Templo.vai())
-        fala = "beleza" if quer == "s" else "que triste"
-        input(fala)
-        _joga_tesouro = JogaTesouro()
-        while _joga_tesouro.joga() == "s":
-            pass
-        fala = "que pena"
-        input(fala)
 
+def tesouro_inca():
+    quer = input(f"Bem vindo ao Tesouro Inca - versão Roxanne {__name__}.Quer jogar (s)?", cena_Templo.vai())
+    fala = "beleza" if quer == "s" else "que triste"
+    input(fala)
+    # while art_joga() == "s":
+    _joga_tesouro = JogaTesouro()
+    while _joga_tesouro.joga() == "s":
+        pass
+    fala = "que pena"
+    input(fala)
+    
 
-    if __name__ == "__main__":
-        tesouro_inca()
+if __name__ == "__main__":
+    tesouro_inca()
+
