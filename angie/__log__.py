393cba58b5f06e9e4417b532cc199f9bf8472c86
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-09 18:33:02 -0300

    Saved angie/__log__.py @2022-08-09 21:33

--- angie/__log__.py
+++ angie/__log__.py
@@ -1533,3 +1533,21 @@ NameError: name 'cena_templo' is not defined
     quer = input(f"Bem vindo ao Tesouro Inca - versão Angie {__name__}.Quer jogar (s)?", cena_templo.vai())
 NameError: name 'cena_templo' is not defined
 '''},
+{'date': 'Tue Aug 09 2022 18:33:01.593 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    tesouro_inca()
+  module <module> line 72
+    quer = input(f"Bem vindo ao Tesouro Inca - versão Angie {__name__}.Quer jogar (s)?", cena_templo.vai())
+NameError: name 'cena_templo' is not defined
+'''},
