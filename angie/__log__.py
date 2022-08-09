096c2a7c21f0d128f78652f76b3a090c264ca08d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-09 17:25:10 -0300

    Saved angie/__log__.py @2022-08-09 20:25

--- angie/__log__.py
+++ angie/__log__.py
@@ -1334,3 +1334,10 @@ SyntaxError: invalid syntax
                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Aug 09 2022 17:25:09.245 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 67
+  if cam(1) = cenas(1)
+             ^
+SyntaxError: invalid syntax
+'''},
