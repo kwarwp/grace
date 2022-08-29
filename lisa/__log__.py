7426e1c853fea0d1e3469ac9d6d3fd5aee6f8d5d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 15:14:08 -0300

    Saved lisa/__log__.py @2022-08-29 18:14

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -880,3 +880,10 @@ SyntaxError: invalid syntax
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 29 2022 15:14:07.612 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  if i = isalpha(MSG)
+        ^
+SyntaxError: invalid syntax
+'''},
