822738e3c20b5a11a6bb8f463c2bdf2da4c5726f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 15:03:54 -0300

    Saved lisa/__log__.py @2022-08-29 18:03

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -788,3 +788,21 @@ Traceback (most recent call last):
     prints (words)
 NameError: name 'prints' is not defined
 '''},
+{'date': 'Mon Aug 29 2022 15:03:53.188 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''103
+203
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    i = i+2
+TypeError: Can't convert int to str implicitely
+'''},
