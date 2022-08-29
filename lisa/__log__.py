4a2b77ac607295678064dbb3dab1bf1db6866828
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 14:22:41 -0300

    Created lisa/__log__.py @2022-08-29 17:22

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 29 2022 14:22:39.482 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  print a word 
+        ^
+SyntaxError: missing parenthesis in call to 'print'
+'''},
