41be3a1fe284fe9486a16bce153adf98a39f7b79
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 15:02:29 -0300

    Saved lisa/__log__.py @2022-08-29 18:02

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -754,3 +754,20 @@ Traceback (most recent call last):
     for j in palavra: 
 TypeError: 'int' object is not iterable
 '''},
+{'date': 'Mon Aug 29 2022 15:02:28.658 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''103
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    prints (words)
+NameError: name 'prints' is not defined
+'''},
