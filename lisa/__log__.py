353631aedd24b054d9ef55e6f31ab8b7db7d1ff0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 15:09:22 -0300

    Saved lisa/__log__.py @2022-08-29 18:09

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -813,3 +813,21 @@ TypeError: Can't convert int to str implicitely
         ^
 SyntaxError: missing parenthesis in call to 'print'
 '''},
+{'date': 'Mon Aug 29 2022 15:09:21.880 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''103
+203
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    print (chr(palavra), end=='')
+NameError: name 'end' is not defined
+'''},
