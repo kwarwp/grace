f575cf7a87dd5fc89c779b3665a1a677bd921f2d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 15:14:10 -0300

    Saved lisa/main.py @2022-08-29 18:14

--- lisa/main.py
+++ lisa/main.py
@@ -3,8 +3,8 @@ MSG = ("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmg
 " gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
 #SUBIR DUAS LETRAS
 print (ord("g"))
-
 for i in MSG:
-   words = ord (MSG)
-
+   words = ord (i)+2
+   #print (i)
+   print (chr(words),end="")
 
