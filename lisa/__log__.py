e1e2ab4b59f83e5da53a11e592f620d2d5793e68
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 14:57:57 -0300

    Saved lisa/__log__.py @2022-08-29 17:57

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -269,3 +269,21 @@ TypeError: Can't convert int to str implicitely
          ^
 SyntaxError: can't assign to function call 
 '''},
+{'date': 'Mon Aug 29 2022 14:57:56.712 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''103
+203
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    palavra = ord (i+2)
+TypeError: Can't convert int to str implicitely
+'''},
