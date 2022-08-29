9e5e73ee3ea2b2a6ee6034c42e8c3cea1e7a4d2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 14:34:13 -0300

    Saved lisa/__log__.py @2022-08-29 17:34

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -93,3 +93,21 @@ Traceback (most recent call last):
     	ord (MSG)
 TypeError: ord() expected a character, but string of length 203 found
 '''},
+{'date': 'Mon Aug 29 2022 14:34:13.141 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''103
+203
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    	ord ('MSG')
+TypeError: ord() expected a character, but string of length 3 found
+'''},
