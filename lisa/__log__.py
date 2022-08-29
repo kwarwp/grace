56c9762d2323b10f3137de3bbc5037e5ffa69d27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 15:38:03 -0300

    Saved lisa/__log__.py @2022-08-29 18:38

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -961,3 +961,21 @@ Exception: <TypeError: this.tree[2] is undefined>
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 29 2022 15:38:02.605 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''103
+203
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    if 96< i <121:
+TypeError: unorderable types: 'str' < int()
+'''},
