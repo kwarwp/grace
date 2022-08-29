130ea984a9e68fc32a27af51a847d88a5983d32b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 15:42:04 -0300

    Saved lisa/main.py @2022-08-29 18:42

--- lisa/main.py
+++ lisa/main.py
@@ -2,13 +2,14 @@
 MSG = ("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb"+
 " gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
 #SUBIR DUAS LETRAS
+MSG = "map"
 print (ord("g"))
 for i in MSG:
    if i.isalpha():
       words = ord (i)+2
       print (chr(words),end="")
    else:
-      print (i)
+      print (i,end="")
        
        
 
