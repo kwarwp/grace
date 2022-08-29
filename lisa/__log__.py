96e74522f4a7e16c8bfb0346818e9a0cb274e356
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-08-29 14:26:23 -0300

    Saved lisa/__log__.py @2022-08-29 17:26

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -20,3 +20,20 @@ SyntaxError: missing parenthesis in call to 'print'
         ^
 SyntaxError: Unbalanced bracket (
 '''},
+{'date': 'Mon Aug 29 2022 14:26:22.942 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''103
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    print (ord('MSG')+2)
+TypeError: ord() expected a character, but string of length 3 found
+'''},
