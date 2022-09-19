554d9160777b50dbd3f45a88bc3dbb16bdd156b8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-19 18:01:15 -0300

    Saved kathryn/__log__.py @2022-09-19 21:01

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -94,3 +94,19 @@ TypeError: sum() takes 2 positional argument but more were given
     for muculmano1 in mulcumano_3vinhos:          
 NameError: name 'mulcumano_3vinhos' is not defined
 '''},
+{'date': 'Mon Sep 19 2022 18:01:14.361 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    st = [([a0, a1, a2], [b0, b1, b2], [c0, c1, c2]) for [a0, a1, a2], [b0, b1, b2], [c0, c1, c2] in s 
+NameError: name 'b3' is not defined
+'''},
