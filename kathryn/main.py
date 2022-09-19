ce23bb442337c504373ea702b80b1c8ce50a5ff7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-19 14:21:53 -0300

    Saved kathryn/main.py @2022-09-19 17:21

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,1 +1,6 @@
 # grace.kathryn.main.py
+"""#]Como pagamento de um pequeno lote de carneiros,
+3 muçulmanos receberam uma partida de vinho,
+composta de 21 vasos iguais, sendo 7cheios, 7 meio-cheios e 7 vazios.
+Querem, agora, dividir os 21 vasos de modo que cada um deles receba a mesma quantidade de vinho.
+"""
