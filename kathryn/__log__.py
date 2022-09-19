03df932979ba683bcde0f59745a7ff1b77459701
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-19 17:39:31 -0300

    Saved kathryn/__log__.py @2022-09-19 20:39

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -60,3 +60,19 @@ NameError: name 'meio_ceio' is not defined
     if cheios + meio_ceio + vazios == 7:
 NameError: name 'meio_ceio' is not defined
 '''},
+{'date': 'Mon Sep 19 2022 17:39:30.901 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    st = [([a0, a1, a2], [b0, b1, b2], [c0, c1, c2]) for [a0, a1, a2], [b0, b1, b2], [c0, c1, c2] in s 
+TypeError: sum() takes 2 positional argument but more were given
+'''},
