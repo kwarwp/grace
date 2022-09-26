ecc225fbef95fdad1f4c2afe2e311cac3f4735a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-26 17:03:04 -0300

    Created natalia/__log__.py @2022-09-26 20:03

---
+++ natalia/__log__.py
@@ -0,0 +1,5 @@
+
+{'date': 'Mon Sep 26 2022 17:03:01.518 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <TypeError: Cannot read properties of undefined (reading 'type')>
+'''},
