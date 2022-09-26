49e2a3279508d7c9a44035cd76b945250a81ce57
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-26 17:19:55 -0300

    Saved natalia/__log__.py @2022-09-26 20:19

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -3,3 +3,10 @@
 'error': '''
 Exception: <TypeError: Cannot read properties of undefined (reading 'type')>
 '''},
+{'date': 'Mon Sep 26 2022 17:19:54.118 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  Manobras = [manobra for j in range(10) if manobra:= valido()] 
+                                                    ^
+SyntaxError: invalid syntax
+'''},
