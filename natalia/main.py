b274c9c39b3462ee7d947f1900de036e46168d0d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-26 17:03:16 -0300

    Saved natalia/main.py @2022-09-26 20:03

--- natalia/main.py
+++ natalia/main.py
@@ -4,6 +4,6 @@ import random
 
 Comandos = [ "direita", "esquerda", "cima", "baixo", "enter"]
 def ManobraTrem():
-    return [random.choice(Comandos) for in range(10)] 
+    return [random.choice(Comandos) for i in range(10)] 
     
 print(ManobraTrem())
