06001233c38bb987ca44622c46a2f4a6b9bb8742
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-26 17:25:46 -0300

    Saved natalia/main.py @2022-09-26 20:25

--- natalia/main.py
+++ natalia/main.py
@@ -5,7 +5,11 @@ import random
 Comandos = [ "direita", "esquerda", "cima", "baixo", "enter"]
 def ManobraTrem():
     def valido():
-        return " ".join(random.choice(Comandos) for i in range(10))
+        Manobra = " ".join(random.choice(Comandos) for i in range(10))
+        if "enter enter" in Manobra: 
+            return None
+        else: 
+            return Manobra
     Manobras = [valido() for j in range(10)] 
     return Manobras
     
