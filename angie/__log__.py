c6495fcf811d941d20a2bf9c8d66569170720355
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-26 12:07:48 -0300

    Saved angie/__log__.py @2022-09-26 15:07

--- angie/__log__.py
+++ angie/__log__.py
@@ -1921,3 +1921,23 @@ AttributeError: 'method' object has no attribute 'revela'
     revela = [cam.revela() for cam in self.criptas]
 AttributeError: 'method' object has no attribute 'revela'
 '''},
+{'date': 'Mon Sep 26 2022 12:07:46.533 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 79
+    tesouro_inca()
+  module <module> line 72
+    while _joga_tesouro.joga() == "s":
+  module <module> line 60
+    revela = [cam.revela() for cam in self.criptas]
+AttributeError: 'method' object has no attribute 'revela'
+'''},
