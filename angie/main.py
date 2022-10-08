18cafeda83a0ba87138c914ced6160a04e9f197e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-10-08 17:44:06 -0300

    Saved angie/main.py @2022-10-08 20:44

--- angie/main.py
+++ angie/main.py
@@ -1,11 +1,10 @@
-   # grace.lisa.main.py
-MSG = ("Que j'aime à faire connaire un nombre utile aux sages Glorieux Archimède artiste ingeneux")
-#SUBIR DUAS LETRAS
-MSG = "map"
-print (ord("g"))
-for i in MSG:
-   if i.isalpha():
-      words = ord (i)+2
-      print (chr(words),end="")
-   else:
-      print (i,end="")
+#Conte as vogais em uma string
+#Crie uma função em Python que aceite uma única palavra e retorne o número de vogais dessa palavra. Nesta função, apenas a, e, i, o e u serão contados como vogais — não y.
+
+def conte_as_vogais(frase):
+    """Conte as vogais.
+    """
+    vogais = [ 'a', 'e', 'i', 'o', 'u']
+    for i in vogais:
+        bonita += frase.count (i)
+    return bonita
