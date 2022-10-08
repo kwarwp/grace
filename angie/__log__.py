e71e435c3a1edf230f1d951505299fb00dfcde4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-10-08 17:56:36 -0300

    Saved angie/__log__.py @2022-10-08 20:56

--- angie/__log__.py
+++ angie/__log__.py
@@ -2000,3 +2000,19 @@ NameError: name 'un' is not defined
     print("Tem " + str(total_occurrences) + " ocorrencias.")
 NameError: name 'total_occurrences' is not defined
 '''},
+{'date': 'Sat Oct 08 2022 17:56:33.528 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    print("Tem " + total_occurrencias + " ocorrencia.")
+TypeError: Can't convert int to str implicitely
+'''},
