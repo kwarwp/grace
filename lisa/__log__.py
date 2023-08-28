f3231df010026a2fdaf894042b01d3d7a46374fc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-08-28 16:29:45 -0300

    Saved lisa/__log__.py @2023-08-28 19:29

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -1068,3 +1068,21 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Aug 28 2023 16:29:44.628 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    vas()
+  module <module> line 43
+    mus1 = sample(vasos,7)
+NameError: name 'sample' is not defined
+'''},
