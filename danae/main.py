a025173896e60255f7b15f0ece7f92fcd0296205
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-11 10:58:57 -0300

    Created danae/main.py @2023-09-11 13:58

---
+++ danae/main.py
@@ -0,0 +1,7 @@
+# grace.danae.main.py
+from browser import document 
+# o nome document aponta para uma lista 
+parte = document["um_texto"]
+# Aqui a lista vai retornar a parte que foi identificada por "um_texto"
+parte.html = "Um texto que escrevi usando o programa Python"
+
