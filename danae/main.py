04ca997678e2b9b9ce066d1d656a6118b7fab0eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-11 11:05:01 -0300

    Saved danae/main.py @2023-09-11 14:05

--- danae/main.py
+++ danae/main.py
@@ -1,7 +1,12 @@
 # grace.danae.main.py
 from browser import document 
-# o nome document aponta para uma lista 
-parte = document["um_texto"]
-# Aqui a lista vai retornar a parte que foi identificada por "um_texto"
-parte.html = "Um texto que escrevi usando o programa Python"
+from browser import svg 
+parte = document["um_desenho"]
+tela = svg.svg(width=400, height=200)
+parte <= tela
+tela <= svg.rect(width=100, height=150, style={'fill': 'red'})
+tela <= svg.circle(cx=200, cy=100, r=80, style={'fill': 'blue'})
+tela <= svg.line(x1=110, y1=30, x2=120, y2=180, style={'stroke': 'green', 'stroke-width': '5px'})
+tela <= svg.path(d="M320 20 l80 80 l-80 80 Z", style={'fill': 'magenta'})
+
 
